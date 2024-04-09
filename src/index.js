5574985d36bd1ae12b360d802ccfcfc5df19a8b8
GosuUHC <GosuUchenik@yandex.ru>
2024-04-09 23:36:42 +0700

    Educator page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { ThemeProvider } from "@material-tailwind/react";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
 );
---
+++ src/view/components/cardlist/card/Card.js
@@ -0,0 +1,22 @@
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+} from "@material-tailwind/react";
+
+const RecCard = ({data}) => {
+  return (
+    <Card className="mt-6 w-96">
+      <CardBody>
+        <Typography variant="h5" color="blue-gray" className="mb-2">
+          {data}
+        </Typography>
+        <Typography>Recommendation to ..... .... ....</Typography>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default RecCard;
